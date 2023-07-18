5762ee50e5d2d8a0223e427ed849e8018d16fc31
msebaf <msebaf@hotmail.com>
2023-07-18 00:27:13 -0300

    modelo comentarios

---
+++ models/comentario.js
@@ -0,0 +1,44 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class comentario extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      
+    }
+  }
+  usuario.init({
+   
+    autorId:{
+       type: DataTypes.INTEGER,
+       allowNull: false
+    },
+    fotoId :{
+      type : DataTypes.INTEGER,
+      allowNull: false
+    },
+    comentario: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    fechaCrecion:{
+      type:DataTypes.DATE,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    timestmaps:false,
+    createdAt:false,
+    updatedAt:false,
+    modelName: 'comentario',
+    tableName:"comentarios"
+  });
+  return comentario;
+};
